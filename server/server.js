1ca28954c4796867f4f0c9e81e7af32fc852a90d
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-10-24 18:21:55 -0400

    added .env constants

--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
-const app = express()
 
+const cors = require("cors")
+const { default: mongoose } = require('mongoose')
+require('dotenv').config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+// DB Connection
 app.get("/api", (req, res)=>{
     res.json({"msg":"hello world!"})
 })
 
-app.listen(5000, ()=>{console.log("server started on port 5000.")})
+//app.listen(5000, ()=>{console.log("server started on port 5000.")})
+app.listen(process.env.PORT, ()=>{console.log("server started on port: "+process.env.PORT+" ...")})
