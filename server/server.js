5f36b134b5bea1b34f0b7ee16ea8ab5740171cd1
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-11-15 17:35:35 -0500

    modified console.log msg

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser : true ,
     useUnifiedTopology : true
 }).then(()=>{
-    console.log('connected to db');
+    console.log('connected to mongoDB atlas instance.');
     /*const userTest = new UserModel({username:'Bastian', email:'bastian.bastias@gmail.com',password:'weCanTalk2023'})
     userTest.save().then(doc => {
         console.log('user saved:', doc);
